47996cf459a4963ea9a1a89cb59b80fad7d37ab6
Mubarek Shikur <manmuba90@gmail.com>
2023-10-02 21:46:51 +0300

    Creating serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from .models import User
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    uername = serializers.CharField(read_only=True)
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
