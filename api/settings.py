de15ee7ce2ddb6ceafedae20e032ccc9eb0e5dcf
Mubarek Shikur <manmuba90@gmail.com>
2023-10-02 21:44:50 +0300

    making migration

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'authentication.User'
 
 # Application definition
 
@@ -114,10 +115,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR /'static',
-]
 
 STATIC_ROOT = BASE_DIR / 'static'
 
