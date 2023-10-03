e40de08f0ce3e67f47c2ce60ce5aeba083cf157c
Mubarek Shikur <manmuba90@gmail.com>
2023-10-03 07:01:31 +0300

    Writting Product and category models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
