06fd15c3f1ad4e58731a73d50eff83e2a5618a87
Mubarek Shikur <manmuba90@gmail.com>
2023-10-03 14:58:10 +0300

    All done

--- api/settings.py
+++ api/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'authentication.User'
 
