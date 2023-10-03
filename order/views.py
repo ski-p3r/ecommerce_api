fcc1f55fe486fb9e749b7c6d39c1e659c045e5a3
Mubarek Shikur <manmuba90@gmail.com>
2023-10-03 10:18:57 +0300

    writting views

--- order/views.py
+++ order/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
 
 # Create your views here.
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        return {'user': self.request.user}
+    
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
