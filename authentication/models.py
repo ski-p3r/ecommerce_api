09c703efd6add2d4135f205f304d714ed5e2605e
Mubarek Shikur <manmuba90@gmail.com>
2023-10-03 07:24:31 +0300

    category and products Done

--- authentication/models.py
+++ authentication/models.py
@@ -29,6 +29,7 @@ class UserManager(BaseUserManager):
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
+        user.is_active = True
         user.save(using=self._db)
         return user
     
